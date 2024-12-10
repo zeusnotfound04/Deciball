485b8deea807a4e1a656f3a9acab510a174cc88e
zeusnotfound04 <zeusnotfound0@gmail.com>
2024-12-10 23:49:32 +0530

    user can add streams

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from './../../lib/db';
 import { NextRequest, NextResponse } from "next/server"
 import {z} from "zod"
-
+const YT_REGEX = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9_-]{11}$")
 
 const CreatStreamSchema = z.object({
     creatorId : z.string(),
@@ -13,10 +13,22 @@ export async function POST(req:NextRequest) {
 
     try {
         const data = CreatStreamSchema.parse(await req.json())
-        prismaClient.stream.create({
-            userId :data.creatorId,
-            
-
+        const isYt = YT_REGEX.test(data.url)
+        if(!isYt){
+            return NextResponse.json({
+                message : "Wrong URL format"
+            }, {
+                status : 441
+            })
+        }
+        const extractedId = data.url.split("?v=")[1]
+        await prismaClient.stream.create({
+            data : {
+                userId :data.creatorId,
+                url : data.url,
+                extractedId,
+                type : "Youtube"
+            }
         })
     } catch (e) {
         return NextResponse.json({
