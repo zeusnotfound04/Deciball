edaaae8d94f6e79f8d0d57f35d935ff9ad9555c0
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-02-23 19:30:56 +0530

    done with the websocket setups

--- ws/src/app.ts
+++ ws/src/app.ts
@@ -3,6 +3,8 @@ import cluster from "cluster";
 import http from "http"
 import dotenv from "dotenv"
 import { RoomManager } from "./streamManager";
+import jwt  from "jsonwebtoken";
+import { sendError } from "./utils";
 
 
 
@@ -42,6 +44,96 @@ function createHttpServer() {
 }
 
 
+async function handleJoinRoom(ws: WebSocket , data : Data){
+    jwt.verify(
+        data.token,
+        process.env.NEXTAUTH_SECRET as string,
+        (err : any , decoded : any) => {
+            if(err){
+                console.log(err)
+                sendError(ws , "Token verification failed")
+            } else {
+                RoomManager.getInstance().joinRoom(
+                    data.spaceId,
+                    decoded.creatorId,
+                    decoded.userId,
+                    ws,
+                    data.token
+                )
+            }
+        }
+    )
+}
+
+
+async function  processUserAction(type: string , data : Data ) {
+    switch (type) {
+        case "cast-vote":
+            await RoomManager.getInstance().casteVote(
+                data.userId,
+                data.streamId,
+                data.vote,
+                data.spaceId
+            )
+            break;
+        
+        case "add-to-queue":
+            await RoomManager.getInstance().addToQueue(
+                  data.spaceId,
+                  data.userId,
+                  data.url
+            );
+            break;
+
+        case "play-next":
+            await RoomManager.getInstance().queue.add("play-next",{
+                    spaceId: data.spaceId,
+                    userId : data.userId
+                } )
+            break;
+
+        case "remove-song":
+            await RoomManager.getInstance().queue.add("remove-song" , {
+                    ...data,
+                    spaceId : data.spaceId,
+                    userId : data.userId
+            })
+            break;
+        
+        case "empty-queue":
+            await RoomManager.getInstance().queue.add("empty-queue", {
+            ...data,
+            spaceId: data.spaceId,
+            userId: data.userId,
+            });
+            break;
+    
+        case "pay-and-play-next":
+            await RoomManager.getInstance().payAndPlayNext(
+            data.spaceId,
+            data.userId,
+            data.url
+            );
+            break;
+  
+    
+        default:
+            console.warn("Unknown message type:" , type);
+    }    
+}
+
+async function handleUserAction(ws:WebSocket , type : string , data : Data) {
+    const user = RoomManager.getInstance().users.get(data.userId);
+
+
+    if (user){
+        data.userId = user.userId;
+        await processUserAction(type , data)
+    } else{
+        sendError(ws, "You are unauthorized to perform this action");
+    }
+}
+
 async function handleConnection(ws:WebSocket) {
     ws.on("message" , async (raw : {toString : ()=> string}) => {
         const {type , data} = JSON.parse(raw.toString()) || {};
@@ -66,7 +158,7 @@ async function main() {
     const server = createHttpServer();
     const wss = new WebSocketServer({server})
     await RoomManager.getInstance().initRedisClient();
-
+    wss.on("connection", (ws) => handleConnection(ws));
     // wss.on("connection", (ws)=> handleConnection(ws))
 
     const PORT = process.env.PORT ?? 8080;
