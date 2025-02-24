920bd70155dda3803746f577894b16955fa95379
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-02-24 23:37:54 +0530

    made the redirect hook

---
+++ next-app/hooks/useRedirect.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { usePathname , useRouter } from "next/navigation";
+
+
+export default function useRedirect(){
+    const session = useSession();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    useEffect(()=>{
+        if (session.status === "unauthenticated") {
+            router.push("/")
+        }
+    } , [session] )
+}
