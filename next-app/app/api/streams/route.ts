4d78509595b1d7a8e76b7c3d373d03f4e9ecb208
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-03-04 07:06:35 +0530

    stream Route : fixed the GET and POST methods

--- next-app/app/api/streams/route.ts
+++ next-app/app/api/streams/route.ts
@@ -2,10 +2,18 @@ import { prisma } from '@/app/lib/db';
 import { NextRequest, NextResponse } from "next/server"
 import {z} from "zod"
 
+//@ts-ignore
 import youtubesearchapi from 'youtube-search-api'
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/lib/auth';
+
+
+
+
 const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 
 const CreateStreamSchema = z.object({
+    spaceId : z.string(),
     creatorId : z.string(),
     url : z.string()
 })
@@ -14,7 +22,21 @@ const CreateStreamSchema = z.object({
 export async function POST(req:NextRequest) {
 
     try {
+        const session = await getServerSession(authOptions)
         const data = CreateStreamSchema.parse(await req.json())
+        
+        
+        if(!session?.user.id){
+            return NextResponse.json(
+                {
+                    message : "Youtube link cannot be empty"
+                },
+                {
+                    status : 400
+                }
+            )
+        }
+       
         
         const isYt = data.url.match(YT_REGEX)
         if(!isYt){
@@ -28,6 +50,61 @@ export async function POST(req:NextRequest) {
 
         const res = await youtubesearchapi.GetVideoDetails(extractedId)
 
+        const user = session.user;
+
+        if (user.id !== data.creatorId){
+            const tenMinutesAgo = new Date(Date.now()- 10 * 60 * 1000 )
+            const twoMinutesAgo = new Date(Date.now() - 2 * 60 * 1000)
+
+            const userRecentStreams = await prisma.stream.count({
+                where : {
+                    userId : data.creatorId,
+                    extractedId : user.id,
+                    createAt : {
+                        gte : tenMinutesAgo,
+                    }
+                }
+            })
+
+            const duplicateSong = await prisma.stream.findFirst({
+                where : {
+                    userId : data.creatorId,
+                    extractedId : extractedId,
+                    createAt : {
+                        gte : tenMinutesAgo,
+                    }
+                }
+            })
+
+            if(duplicateSong){
+                return NextResponse.json(
+                    {
+                        message : "This song was already added in the last 10 minutes"
+                    },
+                    {
+                        status : 429
+                    }
+                )
+            }
+
+            const streamLastTwoMinutes = await prisma.stream.count({
+                where : {
+                    userId : data.creatorId,
+                    addedBy : user.id,
+                    createAt: {
+                        gte : twoMinutesAgo
+                    }
+                }
+            })
+
+            if (userRecentStreams >= 5) {
+                return NextResponse.json({
+                    message : "Rate limit Exceeded: You can only add 5 songs per 10 minutes"
+                } ,{
+                    status : 429
+                })
+            }
+        }
 
         const smallImage = JSON.stringify(res.thumbnail.thumbnails[0].url);
         
@@ -37,11 +114,12 @@ export async function POST(req:NextRequest) {
         const stream = await prisma.stream.create({
             data : {
                 userId :data.creatorId,
+                addedBy : user.id,
                 url : data.url,
                 extractedId,
                 title : res.title,
-                smallImage : smallImage,
-                bigImage : bigImage,
+                smallImg : smallImage,
+                bigImg : bigImage,
                 type : "Youtube"
             }
         })
@@ -49,8 +127,9 @@ export async function POST(req:NextRequest) {
         console.log(stream)
 
         return NextResponse.json({
-            message : "Stream added successfully",
-            id : stream.id
+          ...stream ,
+          hasUpvoted: false,
+          upvotes : 0
         })
     } catch (e) {
         return NextResponse.json({
@@ -62,18 +141,84 @@ export async function POST(req:NextRequest) {
     
 }
 
-
-export async function GET(req :NextRequest) {
-
-    const creatorId = req.nextUrl.searchParams.get("creatorId")
-    const streams = await prisma.stream.findMany({
-        where :{
-            userId : creatorId ?? ""
+export async function GET(req: NextRequest) {
+    const spaceId = req.nextUrl.searchParams.get("spaceId");
+    const session = await getServerSession(authOptions);
+    if (!session?.user.id) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+    const user = session.user;
+  
+    if (!spaceId) {
+      return NextResponse.json({
+          message: "Error"
+      }, {
+          status: 411
+      })
+  }
+  
+    const [space, activeStream] = await Promise.all([
+      prisma.space.findUnique({
+        where: {
+            id: spaceId,
+        },
+        include: {
+            streams: {
+                include: {
+                    _count: {
+                        select: {
+                            upvotes: true
+                        }
+                    },
+                    upvotes: {
+                        where: {
+                            userId: session?.user.id
+                        }
+                    }
+  
+                },
+                where:{
+                    played:false
+                }
+            },
+            _count: {
+                select: {
+                    streams: true
+                }
+            },                
+  
+        }
+        
+    }),
+    prisma.currentStream.findFirst({
+        where: {
+            spaceId: spaceId
+        },
+        include: {
+            stream: true
         }
     })
-
-
+    ]);
+  
+    const hostId =space?.hostId;
+    const isCreator = session.user.id=== hostId
+  
     return NextResponse.json({
-        streams
-    })
-}
+      streams: space?.streams.map(({_count, ...rest}) => ({
+          ...rest,
+          upvotes: _count.upvotes,
+          haveUpvoted: rest.upvotes.length ? true : false
+      })),
+      activeStream,
+      hostId,
+      isCreator,
+      spaceName:space?.name
+  });
+  }
