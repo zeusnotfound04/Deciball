4fd63904bd7107bc91e74d2158cf652cfa3de8ed
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-07-14 02:36:51 +0530

    feat: integrated the spotfiy provider

--- next-app/app/lib/auth.ts
+++ next-app/app/lib/auth.ts
@@ -52,7 +52,10 @@ export const authOptions: NextAuthOptions = {
     strategy: 'jwt',
   },
   providers: [
- 
+     SpotifyProvider({
+    clientId: process.env.SPOTIFY_CLIENT_ID || "",
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET || ""
+  }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
